06a9a664e506cff66c69227135a8c0f6f2f805bd
vincent bonnier <vincent@kollebolle.com>
2020-05-02 13:40:02 +0200

    mise en place guard login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { ChatComponent } from './chat/chat.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   { path: '', component: UserProfileComponent, pathMatch: 'full' },
   { path: 'signin', component: SigninComponent, pathMatch: 'full' },
-  { path: 'chat', component: ChatComponent, pathMatch: 'full' },
+  { path: 'chat', component: ChatComponent,  canActivate: [AuthGuard] },
 ];
 
 @NgModule({
