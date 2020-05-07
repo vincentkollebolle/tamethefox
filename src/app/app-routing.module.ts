a822f894fd27a83eed6f3b2b9f37abc187991485
vincent bonnier <vincent@kollebolle.com>
2020-05-07 11:02:19 +0200

    bugfix: redirection after login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { ChatComponent } from './chat/chat.component';
 import { AuthGuard } from './guard/auth.guard';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: ChatComponent, pathMatch: 'full',  canActivate: [AuthGuard] },
+  { path: '', component: AppComponent, pathMatch: 'full',  canActivate: [AuthGuard] },
   { path: 'signin', component: SigninComponent, pathMatch: 'full' },
   { path: 'chat', component: ChatComponent,  canActivate: [AuthGuard] },
 ];
