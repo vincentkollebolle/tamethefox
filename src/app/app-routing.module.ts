4e3e29ff3c5f6ae0d936d901f7f15c87c661f826
vincent bonnier <vincent@kollebolle.com>
2020-05-07 18:50:04 +0200

    bugfix creation utilisateur et premiere connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 import { ChatComponent } from './chat/chat.component';
 import { AuthGuard } from './guard/auth.guard';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   { path: '', component: AppComponent, pathMatch: 'full',  canActivate: [AuthGuard] },
   { path: 'signin', component: SigninComponent, pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'chat', component: ChatComponent,  canActivate: [AuthGuard] },
 ];
 
--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,9 +1,6 @@
 export interface User {
     uid: string;
     email: string;
-    photoURL?: string;
-    displayName?: string;
-    myCustomData?: string;
-    mood: string;
+    mood?: string;
 }
   
